c5b7ab908090eeb7621ab53e41c04ed12377a1e6
Edson W. Rocha <126734645+edsonwrocha@users.noreply.github.com>
2023-03-10 21:22:08 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -1,22 +1,26 @@
-nome = []
-idade = []
-sexo = []
-soma = 0
+libry = []
 maior = 0
+soma = 0
+f_20 = 0
+nome_maior = 0
 for i in range(0, 4):
-    nome.append(str(input('Digite o nome: ')))
-    idade.append(int(input('Digite a idade: ')))
-    sexo.append(str(input('Digite o sexo: ')))
-
-for i in idade:
-    soma += i
-    media = soma / 4
-
-for i in idade:
-    if i > maior:
-        maior = i
+    user = [str(input('Digite o nome: ')), int(input('Digite a idade: ')), str(input('Digite o sexo: '))]
+    libry.append(user)
 
 for i in range(0, 4):
-    if maior == idade[i]:
-        ind_maior = i
+    soma += libry[i][1]
+print('A média das idades cadastradas é {:.0f}'.format(soma / 4))
+
+for i in range(0, 4):
+    if libry[i][2] == 'm' and libry[i][1] > maior:
+        maior = libry[i][1]
+        nome_maior = libry[i][0]
+print('O homem de maior idade cadastrado é {} e tem {} ano(s)'.format(nome_maior, maior))
+
+for i in range(0, 4):
+    if libry[i][2] == 'f' and libry[i][1] < 20:
+        f_20 += 1
+print('O total de mulheres cadastradas com menos de 20 anos é {}'.format(f_20))
+
+
 
